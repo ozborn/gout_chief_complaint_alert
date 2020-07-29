d43829de96edeef6577f9f27e479865eb77dc799
John Osborne <ozborn@uab.edu>
2020-07-29 14:15:36 -0500

    Updated revisions to replicate.py based on corrections made after revisions

--- replicate.py
+++ replicate.py
@@ -7,15 +7,25 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 import sys
 
+# Data types are Consensus from Chart Review, Predict based off Chief Complaints
+# Synthetic is Predict overwritten with Chart Review if applicable
+# Unified merges Yes and Unknown class into a single yes class
+# SynMerged does the same as Unified but on the Synthetic data set
+data_type="Consensus"
+
 # Input Data File
 if(len(sys.argv)<2):
-    print("Input data file path, ex)CC-2019-CORPUS-PAPER.tsv")
+    print("Input data file path, ex)CC-2019-CORPUS-PAPER.tsv ; 2nd argument is Predict|Consensus (default Consensus)")
     sys.exit(0)
 else:
     corpus = sys.argv[1]
+    if(len(sys.argv)==3):
+        data_type=sys.argv[2]
 
 
 df = pd.read_csv(corpus, delimiter='\t')
+
+
 
 #print(df)
 print("Table 3 Raw Data")
@@ -25,11 +35,32 @@ print(predict)
 print("Gout Flare Status as determined by Chart Review")
 consensus = df['Consensus'].value_counts()
 print(consensus)
+# Create Unified Consensus Chart Review Data joining Y and U classes
+print("Gout Flare Unified Data")
+df['Unified'] = np.where(df['Consensus'].isin(['U']),'Y', df['Consensus'])
+syn = df['Unified'].value_counts()
+print(syn)
+# Create Synthetic Data using CC Predicted unless overwritten by Chart Review Status
+print("Gout Flare Synthetic Data")
+df['Synthetic'] = np.where(df['Consensus'].isin(['-']), df['Predict'], df['Consensus'])
+syn = df['Synthetic'].value_counts()
+print(syn)
+# Create Merging Synthetic Data using CC Predicted unless overwritten by Chart Review Status and Merging Unknowns into Y
+print("Gout Flare Synthetic Merged Data")
+df['SynMerged'] = np.where(df['Synthetic'].isin(['U']), 'Y', df['Synthetic'])
+synmerged = df['SynMerged'].value_counts()
+print(synmerged)
+
+print(df)
+print("Results for "+data_type+" data")
 
 
 # Gout Body Locations
 #Skipping stemming locations, vocabulary is small
-goutBodyLocation = ['ARM','HIP','SHOULDER','WRIST','ANKLE','KNEE','TOE','FOOT','FEET','LEG','ELBOW','FINGER','THUMB']
+#goutBodyLocation = ['ARM','HIP','SHOULDER','WRIST','ANKLE','KNEE','TOE','FOOT','FEET','LEG','ELBOW','FINGER','THUMB']
+goutBodyLocation = ['ARM','HIP','SHOULDER','WRIST','ANKLE','KNEE','TOE','FOOT','FEET','LEG','ELBOW','FINGER','THUMB','HAND']
+#goutBodyLocation = ['ARM','HIP','SHOULDER','WRIST','ANKLE','KNEE','TOE','FOOT','FEET','ELBOW','FINGER','THUMB','HAND']
+#goutBodyLocation = ['HIP','SHOULDER','WRIST','ANKLE','KNEE','TOE','FOOT','FEET','ELBOW','FINGER','THUMB','HAND']
 goutBodyLocationAbbreviations = ['LLE','LUE','RUE','RLE','LE','UE']
 
 # Past Medical History
@@ -38,16 +69,21 @@ pmhx = ['PMH','PMHX','HX','PMX']
 # Gout Keywords from Table 1. https://onlinelibrary.wiley.com/doi/full/10.1002/acr.22324
 gout_keywords=['gout','podagra','tophaceous','tophi','tophus']
 
+sudden_keywords=['sudden','acute','attack','flare','woke','wake']
+swollen_keywords=['swollen','redness','edema','cellulitis','swelling','tender joint']
+pain_keywords=['pain','hurt']
+
 #Alternative Gout Related Keywords from Table 1. https://onlinelibrary.wiley.com/doi/full/10.1002/acr.22324
 alt_keywords=['acute flare','acute inflammatory process','allopurinol','arthritis','attack','big toe','cellulitis',
 'codeine','colchicine','chronic arthritis','corticosteroids', 'diclofenac','edema','elevated levels of uric acid',
-'flare','flare up','flare‐up','g6pd    ','gonagra','high uric acid level','hydrocodone','hyperuricemia','ibuprofen'
+'flare','flare up','flare‐up','g6pd','gonagra','high uric acid level','hydrocodone','hyperuricemia','ibuprofen'
 ,'indomethacin','inflammation of joint','joint pain','kidney stone','king\'s disease','metacarpal',
 'metacarpophalangeal joint','metatarsal phalangeal','metatarsal‐phalangeal','naprosyn','naproxen','nsaid',
-'oxycodone','recu    rrent attacks','red joint','redness and swelling','swelling','swollen joint','synovial biopsy',
-'synovial fluid analysis','tender joint','urate lower     drugs','urate‐lowering therapy','urate nephropathy',
+'oxycodone','recurrent attacks','red joint','redness and swelling','swelling','swollen joint','synovial biopsy',
+'synovial fluid analysis','tender joint','urate lowering drugs','urate‐lowering therapy','urate nephropathy',
 'uric acid','uric acid crystals','uric crystals','voltarol','zyloric'
 ]
+
 
 goutRegex = re.compile('.*gout.*',re.IGNORECASE)
 
@@ -113,6 +149,16 @@ def hasGoutBodyLocationCurrent(cc):
     return False
 
 
+def hasGoutBodyLocationAbbrevCurrent(cc):
+    goutblmatch = re.search("|".join(goutBodyLocationAbbreviations), cc)
+    if(goutblmatch!=None):
+        findgoutbl = goutblmatch.start()
+        if((re.search("|".join(pmhx), cc.upper())) !=None):
+            findpmhx = (re.search("|".join(pmhx), cc.upper())).start()
+            if(findgoutbl<findpmhx):
+                return True
+    return False
+
 
 #################################################
 # Test Code
@@ -160,6 +206,16 @@ def regexGoutBodyLocationAndAnyGoutClassifier(cc):
     return '__label__N'
 
 
+def regexGoutBodyLocationWithAbbrevOrCurrentGoutClassifier(cc):
+    if(goutRegex.match(cc)!=None):
+        if(hasGoutBodyLocationCurrent(cc)):
+            return '__label__Y'
+        if(hasGoutBodyLocationAbbrevCurrent(cc)):
+            return '__label__Y'
+    if(hasGoutCurrent(cc)):
+        return '__label__Y'
+    return '__label__N'
+
 df.rename(columns={'Chief Complaint': 'CC'}, inplace=True)
 
 
@@ -191,7 +247,7 @@ pred_labels=['__label__Y','__label__N','__label__U']
 
 
 #Format data
-data = df[['Consensus', 'CC']].rename(columns={"Consensus":"label", "CC":"text"})
+data = df[[data_type, 'CC']].rename(columns={data_type:"label", "CC":"text"})
 pd.options.display.max_colwidth = 60
 data['label'] = '__label__' + data['label'].astype(str)
 #print(data[1:10])
@@ -218,19 +274,17 @@ title2display='Gout (no PMHx) Classifier (Naive GF)'
 print(title2display)
 showConfusionMatrix(title2display,y_true,y_pred)
 
-#Current Gout Only Classifier
-#y_pred = (pd.DataFrame(data['text']).applymap(regexGoutCurrentClassifier))['text']
-#title2display='Current Gout Classifier'
-#showConfusionMatrix(title2display,y_true,y_pred)
-
 #Current Gout Or Current Body Location Classifier
+goutBodyLocation = ['ARM','HIP','SHOULDER','WRIST','ANKLE','KNEE','TOE','FOOT','FEET','LEG','ELBOW','FINGER','THUMB']
 y_pred = (pd.DataFrame(data['text']).applymap(regexGoutBodyLocationOrCurrentGoutClassifier))['text']
-title2display='Current Gout OR (Gout Body Location And Any Gout) Classifier (SIMPLE-GF)'
+title2display='Current Gout OR (Gout Body Location And Any Gout) Classifier (ORIGINAL SIMPLE-GF without HAND and ABBREV)'
 print(title2display)
 showConfusionMatrix(title2display,y_true,y_pred)
 
-#Gout Or Current Body Location Classifier
-#y_pred = (pd.DataFrame(data['text']).applymap(regexGoutBodyLocationAndAnyGoutClassifier))['text']
-#title2display='Gout Anywhere AND Body Location Classifier'
-#showConfusionMatrix(title2display,y_true,y_pred)
+#Gout Or Current Body Location Abbrev `Classifier
+goutBodyLocation = ['ARM','HIP','SHOULDER','WRIST','ANKLE','KNEE','TOE','FOOT','FEET','LEG','ELBOW','FINGER','THUMB','HAND']
+y_pred = (pd.DataFrame(data['text']).applymap(regexGoutBodyLocationWithAbbrevOrCurrentGoutClassifier))['text']
+title2display='Current Gout OR (Current Gout Body or Gout Body Abbreviation Locations) Classifier (SIMPLE-GF for paper)'
+print(title2display)
+showConfusionMatrix(title2display,y_true,y_pred)
 
